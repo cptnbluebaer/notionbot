74b951afbc7a5a44e6742312174602cbec1e841d
cptnbluebaer <lujo.hoffmann29@gmail.com>
2025-04-01 10:55:58 +0200

    Changing Settings Page + adding some Validation

--- app/addconnection/page.tsx
+++ app/addconnection/page.tsx
@@ -8,9 +8,26 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
 import PropertiesElement from "@/components/propertiesElement";
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+
+type SingleProperty = {
+  id: string;
+  name: string;
+  type: string;
+};
+
+type Properties = {
+  [key: string]: SingleProperty;
+};
+
+type promptInfo = {
+  [key: string]: string;
+};
 
 export default function AddConnection() {
-  const [properties, setProperties] = useState({});
+  const [promptInfo, setPromptInfo] = useState<promptInfo>({});
+  const [properties, setProperties] = useState<Properties>({});
 
   const loadDataBaseHandler = async () => {
     const response = await fetch("/api/addconnection");
@@ -18,14 +35,35 @@ export default function AddConnection() {
     setProperties(dataBaseInfo.properties);
   };
 
-  console.log(Object.entries(properties));
+  const changePromptInfoHandler = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    event.preventDefault();
+    const { value } = event.target;
+
+    setPromptInfo((prevState) => {
+      return {
+        ...prevState,
+        [event.target.id]: event.target.value,
+      };
+    });
+  };
+
+  const submitHandler = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
 
   return (
-    <div className=" dark:bg-[#1A1A1A]  px-4 text-white w-5/6">
+    <form
+      onSubmit={submitHandler}
+      className="dark:bg-[#1A1A1A] px-4 text-white w-5/6"
+    >
       <div className="max-w-4xl mx-auto space-y-8">
-        <Card className=" bg-[#030712] dark:bg-white/10 backdrop-blur-md shadow-md rounded-2xl">
+        <Card className="bg-[#030712] dark:bg-white/10 backdrop-blur-md shadow-md rounded-2xl">
           <CardHeader>
-            <CardTitle className="text-lg text-white">
+            <CardTitle className="text-2xl text-white">
               Add New Database Connection
             </CardTitle>
           </CardHeader>
@@ -33,79 +71,118 @@ export default function AddConnection() {
             <div className="space-y-2 text-white">
               <Label htmlFor="dbid">Notion Database ID</Label>
               <Input
-                className="bg-[#10141E] text-white"
+                pattern="^[A-Za-z0-9]{10,}$"
+                className="bg-[#10141E] text-[#00BCFF]"
                 id="dbid"
                 placeholder="Enter Notion database ID..."
               />
               <Button
+                type="button"
                 onClick={loadDataBaseHandler}
                 className="mt-2 bg-[#364153] hover:bg-[#46546b] text-white"
               >
                 Load Properties
               </Button>
             </div>
-
-            <div className="space-y-2 text-white">
-              <Label htmlFor="command">Telegram Command</Label>
-              <Input
-                className="bg-[#10141E]"
-                id="command"
-                placeholder='e.g. "/task" or "/timetracking"'
-              />
-            </div>
           </CardContent>
         </Card>
 
-        <Card className="bg-[#10141E] dark:bg-white/10 backdrop-blur-md shadow-md rounded-2xl">
-          <CardHeader>
-            <CardTitle className="text-lg text-white">
-              Define Prompt for OpenAI
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Tabs defaultValue="per-property" className="w-full">
-              <TabsList className="bg-gray-200 dark:bg-gray-800">
-                <TabsTrigger value="per-property">
-                  Comment per Property
-                </TabsTrigger>
-                <TabsTrigger value="full-prompt">Write Full Prompt</TabsTrigger>
-              </TabsList>
+        {Object.keys(properties).length <= 0 ? (
+          <></>
+        ) : (
+          <Card className="bg-[#10141E] dark:bg-white/10 backdrop-blur-md shadow-md rounded-2xl">
+            <CardHeader>
+              <CardTitle className="text-lg text-white">
+                Define Prompt for OpenAI
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <Tabs defaultValue="per-property" className="w-full">
+                <TabsList className="bg-gray-200 dark:bg-gray-800">
+                  <TabsTrigger value="per-property">
+                    Comment per Property
+                  </TabsTrigger>
+                  <TabsTrigger value="full-prompt">
+                    Write Full Prompt
+                  </TabsTrigger>
+                </TabsList>
 
-              <TabsContent value="per-property">
-                <p className="text-sm  mb-2 text-white">
-                  Add a description for each property of the database that
-                  OpenAI should use when creating entries.
-                </p>
-                <div className="space-y-4">
-                  {Object.entries(properties).map((element, index) => (
-                    <PropertiesElement
-                      index={index}
-                      label={element[0]}
-                    ></PropertiesElement>
-                  ))}
+                <TabsContent value="per-property">
+                  <p className="text-sm mb-2 text-white">
+                    Add a description for each property of the database that
+                    OpenAI should use when creating entries.
+                  </p>
+                  <div>
+                    {Object.entries(properties).map((element, index) => (
+                      <Badge
+                        key={index}
+                        style={{ animationDelay: `${index * 100}ms` }}
+                        className="m-2 p-3 text-[#00BCFF] bg-[#1D202A] animate-[var(--animate-fade-in-scale)]"
+                      >
+                        {element[0]}
+                      </Badge>
+                    ))}
+                  </div>
+                  <div className="pt-5">
+                    <Textarea
+                      onChange={changePromptInfoHandler}
+                      className="text-white"
+                      id="promptdescription"
+                      placeholder="Describe the prompt OpenAI should follow..."
+                      rows={8}
+                    />
+                  </div>
+                  <Separator className="mt-10" />
+                  <p className="pb-2 text-white pt-4">
+                    Add some additional Info for each field
+                  </p>
+                  <div className="space-y-4">
+                    {Object.entries(properties).map((element, index) => (
+                      <PropertiesElement
+                        key={index}
+                        handler={changePromptInfoHandler}
+                        index={index}
+                        label={element[0]}
+                      />
+                    ))}
+                  </div>
+                </TabsContent>
+
+                <TabsContent value="full-prompt">
+                  <p className="text-sm text-muted-foreground mb-2">
+                    Write a custom prompt that OpenAI should use to fill the
+                    database entry.
+                  </p>
+                  <Textarea
+                    placeholder="Describe the prompt OpenAI should follow..."
+                    className="text-white"
+                    rows={8}
+                  />
+                </TabsContent>
+                <div className="space-y-2 pt-2 text-white">
+                  <Label htmlFor="command">Telegram Command</Label>
+                  <Input
+                    pattern="^\/[A-Za-z]{0,15}$"
+                    className="bg-[#10141E]"
+                    id="command"
+                    required
+                    placeholder='e.g. "/task" or "/timetracking"'
+                  />
                 </div>
-              </TabsContent>
-
-              <TabsContent value="full-prompt">
-                <p className="text-sm text-muted-foreground mb-2">
-                  Write a custom prompt that OpenAI should use to fill the
-                  database entry.
-                </p>
-                <Textarea
-                  placeholder="Describe the prompt OpenAI should follow..."
-                  rows={8}
-                />
-              </TabsContent>
-            </Tabs>
-          </CardContent>
-        </Card>
+              </Tabs>
+            </CardContent>
+          </Card>
+        )}
 
         <div className="flex justify-end">
-          <Button className="bg-[#00BCFF] hover:bg-[#005fb891] text-white">
+          <Button
+            type="submit"
+            className="bg-[#00BCFF] hover:bg-[#005fb891] text-white"
+          >
             Save Connection
           </Button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
--- components/propertiesElement.tsx
+++ components/propertiesElement.tsx
@@ -4,18 +4,28 @@ import { Label } from "./ui/label";
 type MyComponentProps = {
   label: string;
   index: number;
+  handler: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default function PropertiesElement({ label, index }: MyComponentProps) {
+export default function PropertiesElement({
+  label,
+  index,
+  handler,
+}: MyComponentProps) {
   return (
     <div
       className="animate-[var(--animate-fade-in-scale)]"
       style={{ animationDelay: `${index * 100}ms` }}
     >
-      <Label htmlFor="title" className="text-white">
+      <Label htmlFor="title" className="text-[#00BCFF] pb-2">
         {label}
       </Label>
-      <Input id="title" placeholder="Describe this field" />
+      <Input
+        onChange={handler}
+        id={label}
+        placeholder="Describe this field"
+        className="text-white"
+      />
     </div>
   );
 }
